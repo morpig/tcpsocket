87442cd1330821521274e9d09dc75bdb52f055bc
Dito Windyaksa <morpig2@gmail.com>
2023-10-16 23:32:55 +0800

    update

---
+++ client.js
@@ -0,0 +1,71 @@
+const net = require('net');
+const WebSocket = require('ws');
+
+// PORT=3000 HOST=wss://gpu6-0.serverdream.net
+const { PORT, HOST } = process.env;
+
+const server = new net.Server();
+
+// create tcp server
+server.listen(port, () => {
+    console.log(`${new Date()}: waiting for new conns ${PORT} ${HOST}`);
+});
+
+// on new tcp connection
+server.on('connection', (socket) => {
+    socket.setKeepAlive(true);
+
+    // send to buffer until websocket is connected
+    const buffer = [];
+    const ws = new WebSocket(HOST);
+
+    ws.on('open', () => {
+        console.log(`websocket connected`);
+        buffer.forEach((b) => {
+            ws.send(b);
+        });
+        buffer = null;
+    });
+
+    ws.on('message', (data) => {
+        socket.write(data);
+    });
+
+    ws.on('close', (close) => {
+        console.log(`websocket closed`);
+        socket.end();
+    });
+
+    ws.on('error', (err) => {
+        console.log('ws error', err);
+    });
+
+    var first = true;
+    socket.on('data', (chunk) => {
+        if (first) {
+            console.log(chunk.toString());
+            first = false;
+        }
+
+        if (buffer !== null) {
+            buffer.push(chunk);
+            return;
+        }
+
+        if (ws.readyState === WebSocket.OPEN) {
+            ws.send(chunk);
+            return;
+        }
+    })
+
+    socket.on('end', () => {
+        console.log('tcp closed');
+        setTimeout(() => {
+            ws.close();
+        }, 2000)
+    });
+
+    socket.on('error', (err) => {
+        console.log('tcp err', err);
+    })
+});
---
+++ server.js
@@ -0,0 +1,78 @@
+const net = require('net');
+const WebSocket = require('ws');
+
+// PORT=3000 HOST=localhost:3000
+const { PORT, HOST } = process.env;
+
+const wss = new WebSocket.Server({
+    port: PORT,
+    perMessageDeflate: false,
+    skipUTF8Validation: false
+});
+
+// on WS connect -> open TCP connection
+wss.on('connection', (ws, req) => {
+    // create tcp conn, keepalive true
+    const tcpConnection = new net.Socket();
+    tcpConnection.setKeepAlive(true);
+
+    const buffer = [];
+    const hostname = HOST.split(':')[0];
+    const port = HOST.split(':')[1];
+
+    console.log(`${new Date()}: connecting tcp to ${hostname}:${port}`);
+    tcpConnection.connect(hostname, port, () => {
+        console.log(`${new Date()}: connected tcp to ${hostname}:${port}`);
+
+        //send pending WS buffer data -> tcp
+        buffer.forEach((b) => {
+            if (tcpConnection.readyState === 'open') {
+                tcpConnection.write(b);
+            }
+        });
+        buffer = null;
+
+        tcpConnection.on('data', (data) => {
+            //forward tcp data -> ws. validate connection status
+            if (ws.readyState === WebSocket.OPEN) {
+                ws.send(data);
+            }
+        });
+
+        tcpConnection.on('error', (err) => {
+            console.log('tcp error', err);
+        });
+
+        tcpConnection.on('end', (data) => {
+            console.log('tcp closed');
+        });
+    });
+
+    // init -> debug purposes. get first messages
+    let init = true;
+    ws.on('message', (data) => {
+        if (first) {
+            console.log(data.toString());
+        }
+
+        // buffer !== null -> tcp not connected YET
+        if (buffer !== null) {
+            buffer.push(message);
+            return;
+        }
+
+        if (tcpConnection.readyState === 'open') {
+            tcpConnection.write(data);
+        }
+    });
+
+    ws.on('error', (err) => {
+        console.log('ws error', err);
+    });
+
+    ws.on('close', (err) => {
+        if (tcpConnection.readyState === 'open') {
+            tcpConnection.end();
+        }
+    });
+});
