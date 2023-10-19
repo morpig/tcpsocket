a451dbeff8112c384999163a74dc9bda984369cd
Dito Windyaksa <morpig2@gmail.com>
2023-10-19 17:35:18 +0800

    test - upgrade log fix

--- test/uws/server.js
+++ test/uws/server.js
@@ -10,7 +10,7 @@ uws.SSLApp({
 }).ws('/*', {
     sendPingsAutomatically: false,
     upgrade: (res, req, context) => {
-        console.log(`${getCurrentDateTime}: upgrade ${req.getHeader('sec-websocket-extensions')} ${req.getHeader('sec-websocket-protocol')} ${req.getHeader('sec-websocket-key')}`)
+        console.log(`${getCurrentDateTime()}: upgrade extensions=${req.getHeader('sec-websocket-extensions')} protocol=${req.getHeader('sec-websocket-protocol')} key=${req.getHeader('sec-websocket-key')}`)
         // server received upgrade request -> upgrade request to websocket
         res.upgrade(
             {
