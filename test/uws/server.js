749735911fc4a51aa7444b5ffe4fde25be76a5d0
Dito Windyaksa <morpig2@gmail.com>
2023-10-21 01:48:28 +0800

    test - format logs better

--- test/uws/server.js
+++ test/uws/server.js
@@ -11,13 +11,17 @@ const app = uws.SSLApp({
     idleTimeout: 0,
     sendPingsAutomatically: false,
     upgrade: (res, req, context) => {
-        console.log(`${getCurrentDateTime()}: upgrade extensions=${req.getHeader('sec-websocket-extensions')} protocol=${req.getHeader('sec-websocket-protocol')} key=${req.getHeader('sec-websocket-key')} version=${req.getHeader('sec-websocket-version')}`)
+        const id = req.getHeader('x-websocket-id') || req.getQuery('id');
+        const forwardedFor = req.getHeader('cf-connecting-ip') || req.getHeader('x-forwarded-for') || '8.8.8.8';
+        const cfRay = req.getHeader('cf-ray') || 'cfRay';
+
+        console.log(`${getCurrentDateTime()}: ${id} ws upgrade req: cfRay=${cfRay}, remote=${forwardedFor}, extensions=${req.getHeader('sec-websocket-extensions')}, protocol=${req.getHeader('sec-websocket-protocol')}, key=${req.getHeader('sec-websocket-key')}, version=${req.getHeader('sec-websocket-version')}`)
         // server received upgrade request -> upgrade request to websocket
         res.upgrade(
             {
-                'cfRay': req.getHeader('cf-ray'),
-                'forwardedFor': req.getHeader('cf-connecting-ip') || req.getHeader('x-forwarded-for') || '8.8.8.8',
-                id: req.getHeader('x-websocket-id') || req.getQuery('id'),
+                'cfRay': cfRay,
+                'forwardedFor': forwardedFor,
+                id: id,
             },
             req.getHeader('sec-websocket-key'),
             req.getHeader('sec-websocket-protocol'),
@@ -30,7 +34,7 @@ const app = uws.SSLApp({
         ws.subscribe('cfpingtest');
         ws.send(Buffer.from('connect-ping'));
         ws.rawIp = Buffer.from(ws.getRemoteAddressAsText()).toString('utf-8');
-        console.log(`${getCurrentDateTime()}: ${ws.id} connected to ws, remote=${ws.forwardedFor}, cfRay=${ws.cfRay}, rawIp: ${Buffer.from(ws.getRemoteAddressAsText()).toString('utf-8')}`);
+        console.log(`${getCurrentDateTime()}: ${ws.id} ws open: cfRay=${ws.cfRay}, remote=${ws.forwardedFor}, rawIp: ${Buffer.from(ws.getRemoteAddressAsText()).toString('utf-8')}`);
     },
     message: (ws, message, isBinary) => {
         // on websocket receive msg event
@@ -45,7 +49,7 @@ const app = uws.SSLApp({
     },
     close: (ws, code, message) => {
         // on websocket close event
-        console.log(`${getCurrentDateTime()}: ${ws.id} ws closed: ${code} ${Buffer.from(message).toString('utf-8')} ${ws.cfRay} ${ws.rawIp}`)
+        console.log(`${getCurrentDateTime()}: ${ws.id} ws closed: cfRay=${ws.cfRay}, remote=${ws.forwardedFor}, code=${code}, reason=${Buffer.from(message).toString('utf-8')}`)
     }
 }).get('/*', (res, req) => { // opposite!
     res.writeStatus('200 OK').end('OK');
